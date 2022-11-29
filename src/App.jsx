98a378fbad9e5ddff977f0d76ee282cf6f8964d6
KauanMFN <mcaua055@gmail.com>
2022-11-29 17:55:20 -0300

    Commit 1

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+export const App = () => {
+    return (
+        <div className="h-screen flex justify-center items-center">
+            <h1>Olá Mundo!</h1>
+        </div>
+    );
+};
